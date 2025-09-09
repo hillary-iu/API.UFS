5a0a6a06d45d46e4ef95229aa96a2c6bca8d7a2b
hillary <hillary.eduarda@estudante.ifro.edu.br>
2025-09-09 08:07:56 -0400

    cdsuuygadfgauyfg

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
-import colecaoUf from "./dados/dados.js";
+
 import express from 'express';
+import { buscarUfPorId, buscarUFsPorNome, buscarUFs, buscarUFsPorSiglas } from './servicos/servico.js';
 
 const app = express();
 
-app.get ('/ufs', (req, res) =>{
-    res.json(colecaoUf)
+app.get('/ufs', (req, res) => {
+    const nomeUF = req.query.busca;
+    const resultado = nomeUF ? buscarUFsPorNome(nomeUF) : buscarUFs();
+    
+    if (resultado.length > 0 ) {
+        res.json(resultado);
+    }else{
+        res.status(404).send({ "erro": "nenhuma uf encontrada" });
+    }
 });
 
+
 app.get('/ufs/:iduf', (req, res) => {
-    const idUF = parseInt(req.params.iduf);
-    const uf = colecaoUf.find(u => u.id == idUF);
-    res.json(uf);
-})
-
-app.get('/ufs/iduf', (req, res) => { /* */
-    const idUF = parseInt(req.params.iduf);/* */
-    let mensagemErro = '';/* */
-    let uf;/* */
-
-
-    if(!(isNaN(idUF))) {/* se isso n for um numero vai retornar verdadeiro */
-        uf = colecaoUf.find(u => u.id === idUF);/*so serve se passar um numero correto da coleção  e se pro exemplo passarr o campo 28 e n tem retorna um campo vazio*/
-        if(!uf){/*verifica se é verdadeira ou falso */
-            mensagemErro = "UF não encontrada";/* */
-        }
-    }else{
-        mensagemErro = 'Requisição invalida';
-    }/*aqui acaba  */ 
-    
+    const uf = buscarUfPorId(req.params.iduf);
 
     if(uf) {
         res.json(uf);
+    }else if (isNaN(parseInt(req.params.iduf))){
+        res.status(400).send({"erro": "requisição invalida"});
     }else{
-        res.status(404).json({"erro": mensagemErro});
+        res.status(404).send({"erro": "UF não encontrada"});
+    }
+});
+
+app.get('ufs/sigla', (req, res) => {
+    const siglaUF = req.query.busca;
+    const contaa = siglaUF ? buscarUFsPorNome(siglaUF) : buscarUFs();
+
+    if(contaa.length > 0) {
+        res.json(contaa);
+    }else if(isNaN(parseInt(req.params.iduf))){
+        res.status(404).send({"erro": "Uf não encontrada"});
+    }else{
+        res.status(400).send({"erro":"requisiçaõ invalida"})
     }
 });
 
 app.listen(8080, () => {
-    let data = new Date();
-    console.log('servidor iniciado na porta 8080 em: ' + data);
+    console.log('servidor iniciado na porta 8080 em: ' );
 });
 
