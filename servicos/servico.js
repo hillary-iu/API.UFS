1781651ee7185dbe470f53b7969ba3bef56eb3df
hillary <hillary.eduarda@estudante.ifro.edu.br>
2025-09-30 08:05:07 -0400

    lalala

--- servicos/servico.js
+++ servicos/servico.js
@@ -1,21 +1,30 @@
 import colecaoUf from "../dados/dados.js";
 
-//Retorna toda coleção
-export const buscarUFs = () => {
-    return colecaoUf;
+// Retorna toda coleção
+export const buscarUfs = () => {
+return colecaoUf;
 }
 
-//retorna UF pelo nome
-export const buscarUFsPorNome = (nomeUf) => {
-    return colecaoUf.filter(uf => uf.nome.toLowerCase().includes(nomeUf.toLowerCase()));
+// Busca Uf pelo nome
+export const buscarUfsPorNome = (nomeUf) => {
+return colecaoUf.filter(uf => uf.nome.toLowerCase().includes(nomeUf.toLowerCase())
+);
 };
 
+// Retorna o id especificado
 export const buscarUfPorId = (id) => {
-    const idUF = parseInt(id);
-    return colecaoUf.find(uf => uf.id === idUF);
+const idUF = parseInt(id);
+return colecaoUf.find(uf => uf.id === idUF);
 }
 
-// Retorna UFs pela sigla
-export const buscarUFsPorSigla = (siglaUf) => {
-    return colecaoUf.filter(uf => uf.sigla.toLowerCase().includes(siglaUf.toLowerCase()));
+// Buscar uf por sigla
+export const buscarUfPorSigla = (siglaUf) => {
+return colecaoUf.find(uf => uf.uf.toLowerCase() === siglaUf.toLowerCase()
+);
+}
+
+// Listar ufs pela inicial do nome
+export const listarPorInicial = (inicialUf) => {
+return colecaoUf.filter(uf => uf.nome.toLowerCase().startsWith(inicialUf.toLowerCase())
+);
 }
