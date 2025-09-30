e808610af3d41a1b86691e47c097ea302e07d914
hillary <hillary.eduarda@estudante.ifro.edu.br>
2025-09-30 07:46:11 -0400

    llallala

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/ufs', (req, res) => {
 const nomeUf = req.query.busca;
 const resultado = nomeUf ? buscarUFsPorNome(nomeUf) : buscarUFs();
 const siglauf = req.query.buscar;
-const resultad_uf =siglauf ? buscarUfsPorSigla(siglauf) : buscarUFs();
+const resultad_uf =siglauf ? buscarUFsPorSigla(siglauf) : buscarUFs();
 
 if (resultado.length > 0) {
 res.json(resultado);
@@ -19,7 +19,7 @@ res.status(404).send({"erro": "nenhuma UF encontrada"});
 
 app.get('/ufs/:siglauf', (req, res) => {
 const siglauf = req.params.siglauf;
-const resultado_uf = buscarUfsPorSigla(siglauf);
+const resultado_uf = buscarUFsPorSigla(siglauf);
 
 
 if (resultado_uf) {
@@ -33,7 +33,7 @@ res.status(404).send({ "erro": "UF não encontrada"});
 
 app.get('/ufs/:iduf', (req, res) => {
 const idUF = req.params.iduf
-const uf = buscarUfsPorId(idUf);
+const uf = buscarUfsPorId(idUF);
 
 
 if (uf) {
